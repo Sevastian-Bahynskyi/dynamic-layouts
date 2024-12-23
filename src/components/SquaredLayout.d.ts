7389e1a00a096438f17c46b78b385a362a63eb58
Sevastian-Bahynskyi <baginski.play@gmail.com>
2024-12-23 11:55:31 +0100

    import is available

---
+++ src/components/SquaredLayout.d.ts
@@ -0,0 +1,4 @@
+import { SquaredLayoutProps } from "../props/squaredLayoutProps";
+declare const SquaredLayout: React.FC<SquaredLayoutProps>;
+export { SquaredLayout };
+export type { SquaredLayoutProps };
---
+++ src/props/squaredLayoutProps.d.ts
@@ -0,0 +1,16 @@
+export interface SquaredLayoutProps {
+    paintedSquareRatio: number;
+    square: {
+        defaultColor: string;
+        changeToDefaultColorInterval: {
+            from: number;
+            to: number;
+        };
+        changeTransition: string;
+    };
+    net: {
+        width: number;
+        color: string;
+    };
+    colorScheme?: string[];
+}

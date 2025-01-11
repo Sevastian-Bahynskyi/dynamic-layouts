c4fb64b41407a3ce34ae53e618dd74b9cff0e5c9
Sevastian Bahynskyi <114170185+Sevastian-Bahynskyi@users.noreply.github.com>
2025-01-11 15:28:43 +0100

    Feature/layout/blobs (#14)
    * feat: blob layout
    
    * fix: pipeline
    
    * fix: rm github package release/publish

--- release.config.cjs
+++ release.config.cjs
@@ -1,21 +1,14 @@
 module.exports = {
-    branches: ["main"],
-    plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/changelog",
-      [
-        "@semantic-release/npm",
-        {
-          npmPublish: true,
-        },
-      ],
-      [
-        "@semantic-release/github",
-        {
-          assets: ["CHANGELOG.md"],
-        },
-      ],
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: true,
+      },
     ],
-  };
-  
+  ],
+};

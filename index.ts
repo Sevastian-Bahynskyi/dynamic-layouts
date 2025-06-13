1eca5d0cb4daa2ea19b61897451460a96451b5a8
Sevastian Bahynskyi <baginski.play@gmail.com>
2025-06-13 21:40:33 +0200

    feat: expose blob in the package #1

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import './index.css';
 export { SquaredLayout } from './src/components/SquaredLayout/SquaredLayout';
 export type { SquaredLayoutConfig } from './src/types/squaredLayout';
-export { BlobLayout } from './src/components/BlobLayout/BlobLayout';
+export { BlobLayout, Blob } from './src/components/BlobLayout/BlobLayout';
 export type { BlobLayoutConfig } from './src/types/blobLayout';
